a4682411e58a1c33624ef0608369a827766c229e
amarbrkic <amar.brkic202@gmail.com>
2022-01-28 20:11:22 +0100

    .

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <string>
+#include <array>
+#include <algorithm>
+
+class Person {
+private:
+  uint8_t age=8;
+
+public:
+  Person(uint8_t);
+  ~Person();
+  uint8_t getAge();
+};
+
+Person::Person(uint8_t age) { this->age = age; }
+
+Person::~Person() {}
+
+uint8_t Person::getAge() { return this->age; }
 
 int main() {
-  for (auto i : "hello world") {
-    std::cout << i << "\n";
-  }
+  Person* Amar = new Person(55);
+
+  std::cout << Amar->getAge() << "\n";
 
   return EXIT_SUCCESS;
 }
